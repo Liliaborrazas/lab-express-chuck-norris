2683f53a20e18d447eacc5122b374e7bcda8c257
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-01-14 18:02:31 +0100

    feat:first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,19 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random',(req,res) =>{
+  client.getRandomJoke()
+  .then (( response ) => {
+     console.log(response)
+   res.render('random',{
+     newJoke: response.value
+   })
+  }). catch (( err ) => {
+      console.log(error)
+  });
+})
+
+app.listen(3000)
